245b887968306e6b8d0c86dbc2f650ee80efaf7e
Vinit-R-Gangurde-Daily <vinitgangurde646@gmail.com>
2025-01-02 14:59:40 +0530

    first design of wings add

--- p1.java
+++ p1.java
@@ -3,6 +3,7 @@ class p1 {
     public static void main(String[] args) {
         
             System.out.println("here is my Second genration fighter get");
+            System.out.println("here we work on wings of get in main branch");
 
 
     }
