d4208ae7220d3eb23e2bb86537415edf0634ae22
Vinit-R-Gangurde-Daily <vinitgangurde646@gmail.com>
2025-01-02 15:03:35 +0530

    additing the funtion of wings

--- p1.java
+++ p1.java
@@ -1,9 +1,21 @@
+
+
+
+
 class p1 {
 
+    void fun(){
+        
+        System.out.println("here i am in engine sector operate over a feature1");
+        
+    }
     public static void main(String[] args) {
         
             System.out.println("here is my Second genration fighter get");
-            System.out.println("here i am in engine sector operate over a feature1");
+        
+            p1 obj1=new p1();
+
+            obj1.fun();
 
 
     }
