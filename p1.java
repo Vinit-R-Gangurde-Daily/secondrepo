032d24418090572ec249a8e5e18c1b709a4ea8bd
Vinit-R-Gangurde-Daily <vinitgangurde646@gmail.com>
2025-01-02 14:58:20 +0530

    first design of engine add

--- p1.java
+++ p1.java
@@ -3,6 +3,7 @@ class p1 {
     public static void main(String[] args) {
         
             System.out.println("here is my Second genration fighter get");
+            System.out.println("here i am in engine sector operate over a feature1");
 
 
     }
